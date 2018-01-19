684644a82e6b7265e1a6781fec3ec6026f1f9b57
Ishaan Shah <ishaan.n.shah@gmail.com>
2018-01-19 14:05:58 +0530

    Updated CHEFGP

--- Codechef/LONG-CHALLENGE/OCT-17/CHEFGP.cpp
+++ Codechef/LONG-CHALLENGE/OCT-17/CHEFGP.cpp
@@ -4,56 +4,74 @@
 */
 
 
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int main()
-{
-    int t, x, y;
-    string s;
+int main() {
+    int t, x, y, len, i, count_a, count_b;
     cin >> t;
-    while(t--)
-    {
-        cin >> s;
+    string str;
+    while (t--) {
+        count_a = 0;
+        count_b = 0;
+        cin >> str;
         cin >> x >> y;
-        vector<char> answer;
-        int a = 0;
-        int b = 0;
-        for(const char &i: s)
-        {
-            if(a == x && i == 'a')
-            {
-                answer.push_back('*');
-                answer.push_back('a');
-                a = 0;
-                b = 0;
-            }
-            else if(b == y && i == 'b')
-            {
-                answer.push_back('*');
-                answer.push_back('b');
-                a = 0;
-                b = 0;
-            }
-            else if(i == 'a')
-            {
-                answer.push_back('a');
-                a++;
-                b = 0;
-            }
-            else if(i == 'b')
-            {
-                answer.push_back('b');
-                b++;
-                a = 0;
-            }
+        len = int(str.length());
+        for (i = 0; i < len; i++) {
+            if (str[i] == 'a')
+                count_a++;
+            else if (str[i] == 'b')
+                count_b++;
         }
-        for(auto elem: answer)
-        {
-            cout << elem;
+
+        if (count_a > count_b) {
+            while (count_a > count_b) {
+                for (i = 0; i < x; i++) {
+                    cout << "a";
+                    count_a--;
+                    if (count_a == count_b)
+                        break;
+                }
+                if (count_a == count_b)
+                    break;
+                else if (count_b == 0 && count_a != 0)
+                    cout << "*";
+                else if (count_b != 0) {
+                    cout << "b";
+                    count_b--;
+                }
+            }
+            while (count_a != 0) {
+                cout << "ba";
+                count_a--;
+            }
+        } else if (count_b > count_a) {
+
+            while (count_b > count_a) {
+                for (i = 0; i < y; i++) {
+                    cout << "b";
+                    count_b--;
+                    if (count_a == count_b)
+                        break;
+                }
+                if (count_a == count_b)
+                    break;
+                if (count_a == 0 && count_b != 0) {
+                    cout << "*";
+                } else if (count_a != 0) {
+                    cout << "a";
+                    count_a--;
+                }
+            }
+            while (count_b != 0) {
+                cout << "ab";
+                count_b--;
+            }
+        } else if (count_b == count_a) {
+            for (i = 0; i < count_a; i++)
+                cout << "ab";
         }
-        cout << endl;
+        cout << "\n";
     }
 }
--- ZCO Practice/README.md
+++
@@ -1,2 +0,0 @@
-# ZCO-Practice
-Solutions for previous year ZCO problems written in python and C++
