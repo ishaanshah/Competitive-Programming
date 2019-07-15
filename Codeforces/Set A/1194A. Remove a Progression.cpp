45b691c8bb4ee87a7293d0273e14edff0dca83e6
ishaanshah <ishaan.n.shah@gmail.com>
2019-07-15 15:43:46 +0530

    Added soltion to problem "Remove a Progression".

---
+++ Codeforces/Set A/1194A. Remove a Progression.cpp
@@ -0,0 +1,19 @@
+/* Created by Ishaan Shah on 14-07-2019.
+* Problem Name: Remove a Progression
+* Problem Link: https://codeforces.com/contest/1194/problem/A
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ll n, x, t;
+    cin >> t;
+    while (t--) {
+        cin >> n >> x;
+        cout << x*2 << endl;
+    }
+}
