ac1f95204bd5f4c41595327b69f3d312ff0135b2
Soham Khadilkar <40205572+soham1001@users.noreply.github.com>
2019-10-03 15:07:55 +0530

    Update sign.cpp

--- INOI/sign.cpp
+++ INOI/sign.cpp
@@ -1,3 +1,12 @@
+// No problem link
+//problem Description
+/*Given a set of n segments {[a 0 , b 0 ], [a 1 , b 1 ], . . . , [a n−1 , b n−1 ]} with integer coordinates on a line, find
+the minimum number m of points such that each segment contains at least one point. That is, find a
+set of integers X of the minimum size such that for any segment [a i , b i ] there is a point x ∈ X such
+that a i ≤ x ≤ b i .*/
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
