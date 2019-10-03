84f39510b72f79d5cbe5de51322a2728b986810b
Soham Khadilkar <40205572+soham1001@users.noreply.github.com>
2019-10-03 15:09:45 +0530

    Update sign.cpp

--- INOI/sign.cpp
+++ INOI/sign.cpp
@@ -1,5 +1,6 @@
 // No problem link
-//problem Description
+//coursera course problem on greedy algorithm
+//Following is the problem Description
 /*Given a set of n segments {[a 0 , b 0 ], [a 1 , b 1 ], . . . , [a n−1 , b n−1 ]} with integer coordinates on a line, find
 the minimum number m of points such that each segment contains at least one point. That is, find a
 set of integers X of the minimum size such that for any segment [a i , b i ] there is a point x ∈ X such
